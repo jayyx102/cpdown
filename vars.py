d83c8e6f6a2e0ed9ac506115594834ffac62895b
jayyx102 <agarwaljaydeep5@gmail.com>
2025-03-24 11:45:12 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,5 +1,5 @@
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
+api_id = 23073061
+api_hash = "006fc5f282b34089f90c6944f97eca76"
 bot_token = "6948815452:AAHLdIf8gKLDp_MMkt8iSJ6wLOTdsAsIq-I"
 auth_users = [1713924419]
 sudo_user = 1713924419
